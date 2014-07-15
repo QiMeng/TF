c0b26698bef104f55ebd893ccbabedd4280f0031
QiMeng <616684889@qq.com>
2014-07-15 08:34:02 +0800

    模块搭建

--- Typhoon/Typhoon/ViewController.h
+++ Typhoon/Typhoon/ViewController.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-
-@interface ViewController : UIViewController
+#import "QMMapViewController.h"
+@interface ViewController : QMMapViewController
 
 @end
--- Typhoon/Typhoon/ViewController.m
+++ Typhoon/Typhoon/ViewController.m
@@ -18,7 +18,140 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+    [self initBottom];
+    
 }
+#pragma mark - 底层视图
+- (void)initBottom {
+    
+    //创建底部视图
+    UIView *  bottomView = [[UIView alloc]initWithFrame:CGRectMake(0, self.view.height - 49, 49*4+20, 49)];
+    bottomView.center = CGPointMake(self.view.center.x, bottomView.center.y);
+    bottomView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin;
+    [self.view addSubview:bottomView];
+    
+    //创建底部视图背景
+    UIImageView * bottomBg = [[UIImageView alloc]initWithFrame:CGRectMake(0, bottomView.height/2, bottomView.width, bottomView.height/2)];
+    bottomBg.image = [UIImage imageAliquotsTensile:kBottomBg];
+    bottomBg.backgroundColor = bottomBg.image?[UIColor clearColor]:[UIColor redColor];
+    [bottomView addSubview:bottomBg];
+    
+    float btnWidth = (bottomBg.width - 20)/4;
+    
+    UIImage * pImage = [UIImage imageNamed:kBottomIcon_Taifeng_n];
+    
+    //创建底部台风按钮
+    UIButton * taifengBtn = [UIButton allocButtonFrame:CGRectMake(10, 0, btnWidth, bottomView.height)
+                                           normalTitle:pImage?@"":@"台风"
+                                         selectedTitle:@""
+                                      normalTitleColor:nil
+                                    selectedTitleColor:nil
+                                       backgroundColor:pImage?[UIColor clearColor]:[UIColor yellowColor]
+                                             titleFont:nil
+                                           normalImage:[UIImage imageNamed:kBottomIcon_Taifeng_n]
+                                         selectedImage:[UIImage imageNamed:kBottomIcon_Taifeng_s]
+                                         normalBgImage:nil
+                                       selectedBgImage:nil
+                                                target:self
+                                              selector:@selector(taifengBtn:)
+                                      autoresizingMask:UIViewAutoresizingNone];
+    [bottomView addSubview:taifengBtn];
+    
+    //创建底部海区预报
+    UIButton * haiquBtn = [UIButton allocButtonFrame:CGRectMake(taifengBtn.right, taifengBtn.top, btnWidth, bottomView.height)
+                                           normalTitle:pImage?@"":@"海区"
+                                         selectedTitle:@""
+                                      normalTitleColor:nil
+                                    selectedTitleColor:nil
+                                       backgroundColor:pImage?[UIColor clearColor]:[UIColor purpleColor]
+                                             titleFont:nil
+                                           normalImage:[UIImage imageNamed:kBottomIcon_Haiqu_n]
+                                         selectedImage:[UIImage imageNamed:kBottomIcon_Haiqu_s]
+                                         normalBgImage:nil
+                                       selectedBgImage:nil
+                                                target:self
+                                              selector:@selector(haiquBtn:)
+                                      autoresizingMask:UIViewAutoresizingNone];
+    [bottomView addSubview:haiquBtn];
+
+    //创建底部预警
+    UIButton * yujingBtn = [UIButton allocButtonFrame:CGRectMake(haiquBtn.right, haiquBtn.top, btnWidth, bottomView.height)
+                                         normalTitle:pImage?@"":@"预警"
+                                       selectedTitle:@""
+                                    normalTitleColor:nil
+                                  selectedTitleColor:nil
+                                     backgroundColor:pImage?[UIColor clearColor]:[UIColor brownColor]
+                                           titleFont:nil
+                                         normalImage:[UIImage imageNamed:kBottomIcon_Yujing_n]
+                                       selectedImage:[UIImage imageNamed:kBottomIcon_Yujing_s]
+                                       normalBgImage:nil
+                                     selectedBgImage:nil
+                                              target:self
+                                            selector:@selector(yujingBtn:)
+                                    autoresizingMask:UIViewAutoresizingNone];
+    [bottomView addSubview:yujingBtn];
+
+    //创建底部天气
+    UIButton * tianqiBtn = [UIButton allocButtonFrame:CGRectMake(yujingBtn.right, yujingBtn.top, btnWidth, bottomView.height)
+                                          normalTitle:pImage?@"":@"天气"
+                                        selectedTitle:@""
+                                     normalTitleColor:nil
+                                   selectedTitleColor:nil
+                                      backgroundColor:pImage?[UIColor clearColor]:[UIColor magentaColor]
+                                            titleFont:nil
+                                          normalImage:[UIImage imageNamed:kBottomIcon_Yujing_n]
+                                        selectedImage:[UIImage imageNamed:kBottomIcon_Yujing_s]
+                                        normalBgImage:nil
+                                      selectedBgImage:nil
+                                               target:self
+                                             selector:@selector(tianqiBtn:)
+                                     autoresizingMask:UIViewAutoresizingNone];
+    [bottomView addSubview:tianqiBtn];
+    
+}
+
+#pragma mark - 底部 台风
+- (void)taifengBtn:(UIButton *)btn {
+    btn.selected = !btn.selected;
+    
+    if (btn.selected) {
+        DLog(@"选中台风");
+    }else {
+        DLog(@"没选中台风");
+    }
+    
+}
+#pragma mark - 底部 海区
+- (void)haiquBtn:(UIButton *)btn {
+    btn.selected = !btn.selected;
+    if (btn.selected) {
+        DLog(@"选中台风");
+    }else {
+        DLog(@"没选中台风");
+    }
+}
+#pragma mark - 底部 预警
+- (void)yujingBtn:(UIButton *)btn {
+    btn.selected = !btn.selected;
+    if (btn.selected) {
+        DLog(@"选中台风");
+    }else {
+        DLog(@"没选中台风");
+    }
+}
+
+#pragma mark - 底部 天气
+- (void)tianqiBtn:(UIButton *)btn {
+    btn.selected = !btn.selected;
+    if (btn.selected) {
+        DLog(@"选中台风");
+    }else {
+        DLog(@"没选中台风");
+    }
+}
+
 
 - (void)didReceiveMemoryWarning
 {
